94c71ddf441090c981297c02d8be31d22c51cba9
madwilliam <madwilliam5225@gmail.com>
2022-09-20 15:42:34 -0800

    update

--- LineScan/run_analysis.m
+++ LineScan/run_analysis.m
@@ -1,6 +1,6 @@
-raw_data_path = 'Z:\Data and Analysis\Data\Two Photon Directory\CBF Data\test\';
-tiff_path = 'Z:\Data and Analysis\Data\Two Photon Directory\CBF Data\test\';
-out_put_dir='Z:\Data and Analysis\Data\Two Photon Directory\CBF Data\test\';
+raw_data_path = 'C:\Users\Montana\Documents\Server_Data and Analysis\CBF Data\';
+tiff_path = 'C:\Users\Montana\Documents\Server_Data and Analysis\AutorunOutput\';
+out_put_dir='C:\Users\Montana\Documents\Server_Data and Analysis\AutorunOutput\';
 meta_files = FileHandler.get_meta_files(raw_data_path);
 tif_files = FileHandler.get_tif_files(tiff_path);
 pmt_files = FileHandler.get_pmt_files(raw_data_path);
