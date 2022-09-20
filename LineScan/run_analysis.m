cd9125446656a08a7c82bb9253285fa5ce28fd30
madwilliam <madwilliam5225@gmail.com>
2022-09-20 13:23:48 -0800

    update

--- LineScan/run_analysis.m
+++ LineScan/run_analysis.m
@@ -1,10 +1,10 @@
-raw_data_path = 'C:\Users\Montana\Documents\Server_Data and Analysis\CBF Data\';
-tiff_path = 'C:\Users\Montana\Documents\Server_Data and Analysis\AutorunOutput\';
-out_put_dir='C:\Users\Montana\Documents\Server_Data and Analysis\AutorunOutput\';
+raw_data_path = 'Z:\Data and Analysis\Data\Two Photon Directory\CBF Data\test\';
+tiff_path = 'Z:\Data and Analysis\Data\Two Photon Directory\CBF Data\test\';
+out_put_dir='Z:\Data and Analysis\Data\Two Photon Directory\CBF Data\test\';
 meta_files = FileHandler.get_meta_files(raw_data_path);
 tif_files = FileHandler.get_tif_files(tiff_path);
 pmt_files = FileHandler.get_pmt_files(raw_data_path);
 
-% crop_tiffs(meta_files,tif_files,pmt_files,tiff_path)
+crop_tiffs(meta_files,tif_files,pmt_files,tiff_path)
 generate_analysis_result(meta_files,tif_files,pmt_files,out_put_dir)
 %generate spike files
