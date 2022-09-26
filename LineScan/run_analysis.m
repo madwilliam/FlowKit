ee1def4c576b4c9d7743ebfe89c0c6d70769da62
madwilliam <madwilliam5225@gmail.com>
2022-09-26 09:51:21 -0800

    fixes

--- LineScan/run_analysis.m
+++ LineScan/run_analysis.m
@@ -1,6 +1,7 @@
-raw_data_path = 'Y:\Zhongkai\Zhongkai Errors';
+raw_data_path = 'Y:\Data and Analysis\Data';
 out_put_dir='C:\Users\dklab\Desktop\test';
 meta_files = FileHandler.get_meta_files(raw_data_path);
 pmt_files = FileHandler.get_pmt_files(raw_data_path);
 pmtToTiff( pmt_files, meta_files, out_put_dir )
 generate_analysis_result(out_put_dir)
+
