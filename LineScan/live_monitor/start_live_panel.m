4a9b8bf75770e43b4982f00346084abada18693e
madwilliam5225 <madwilliam5225@gmail.com>
2022-11-22 16:07:27 -0800

    updates

--- LineScan/live_monitor/start_live_panel.m
+++ LineScan/live_monitor/start_live_panel.m
@@ -1,9 +1,9 @@
 function panel = start_live_panel(hSI)
     function raw_data = get_live_data_feed(~)
         lastStripe=hSI.hDisplay.stripeDataBuffer{hSI.hDisplay.stripeDataBufferPointer};
-        raw_data = lastStripe.rawData(:,2);
+        raw_data = lastStripe.rawData(:,1);
     end
-    panel =  LineScanPanel(@get_live_data_feed);
+    panel =  LineScanPanel(@get_live_data_feed, hSI.hScan2D.lineScanSamplesPerFrame, 100);
 end
 
 
