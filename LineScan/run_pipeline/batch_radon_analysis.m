c1774ef4f9310caf25add59849cd11783fa70f05
madwilliam5225 <madwilliam5225@gmail.com>
2022-12-12 17:06:39 -0800

    upd

--- LineScan/run_pipeline/batch_radon_analysis.m
+++ LineScan/run_pipeline/batch_radon_analysis.m
@@ -4,8 +4,8 @@ meta_files = FileHandler.get_meta_files(raw_data_path);
 pmt_files = FileHandler.get_pmt_files(raw_data_path);
 pmtToTiff( pmt_files, meta_files, out_dir );
 update_start_and_end_time( pmt_files, meta_files, out_dir );
-% % annalyzer = RadonAnnalyzer(@two_step_radon,0.25);
-% % annalyzer.run_batch_radon_analysis(out_dir);
-annalyzer = RadonAnnalyzer(@roi_radon,1);
+annalyzer = RadonAnnalyzer(@two_step_radon,0.25);
 annalyzer.run_batch_radon_analysis(out_dir);
+% annalyzer = RadonAnnalyzer(@roi_radon,1);
+% annalyzer.run_batch_radon_analysis(out_dir);
 
